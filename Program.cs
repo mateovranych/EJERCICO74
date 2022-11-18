af0c0256ae0e6ffb3b749df5608c28052ede7842
mateovranych <69678921+mateovranych@users.noreply.github.com>
2022-11-18 02:13:51 -0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EJERCICO74
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String palabra;
+
+            Stack pila_palabras = new Stack();
+            Console.WriteLine("ingrese la primera palabra a la pila:");
+            palabra = Console.ReadLine();
+
+            while (palabra != "salir")
+            {
+                pila_palabras.Push(palabra);
+                Console.WriteLine("Ingrese la próxima palabra, ingrese ¨salir¨ para salir");
+                palabra = Console.ReadLine();
+            }
+
+            if (palabra == "salir")
+            {
+                Console.WriteLine("La pila generada es:");
+            }
+
+            while (pila_palabras.Count > 0)
+            {
+
+                string texto = (string)pila_palabras.Pop();
+                Console.WriteLine(texto);
+            }
+
+            Console.ReadKey();
+
+
+        }
+
+    }
+}
